6e003723b63eba77cdde4f093a5b1a41c7209cfc
thalisvn <thalisvn2003@gmail.com>
2026-08-26 11:55:20 -0300

    refactor: correções

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -30,7 +30,7 @@ export default function Sobre() {
           </div>
           <div className="border border-border rounded-xl overflow-hidden bg-surface max-w-[320px] md:max-w-none">
             <img
-              src="src/assets/img/perfil.jpeg"
+              src="src\assets\img\perfil.jpeg"
               alt="Foto de Thalis"
               className="w-full block grayscale-[30%] contrast-[1.05]"
             />
